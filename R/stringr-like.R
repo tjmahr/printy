5028b1812d715931bc2687a55ccb04b1195ef373
TJ Mahr <mahr@wisc.edu>
2019-03-28 08:50:35 -0500

    document stringr like functions

--- R/stringr-like.R
+++ R/stringr-like.R
@@ -1,11 +1,33 @@
+#' Break a string to individual (character) tokens
+#'
+#' The usual job of this function is to break a string into a vector of
+#' individual characters, but it can break strings using other separators.
+#'
+#' @param string a character vector of strings to break
+#' @param pattern pattern to use for splitting. Defaults to `NULL` so that
+#'   strings are split into individual characters.
+#' @return a single character vector of the tokens
 #' @export
-str_tokenize <- function(string) {
-  string %>%
-    strsplit(NULL) %>%
-    unlist()
+#' @examples
+#' str_tokenize(c("abc", "de"))
+#' str_tokenize(c("abc de fg"), " ")
+str_tokenize <- function(string, pattern = NULL) {
+  unlist(strsplit(string, split = pattern, perl = TRUE))
 }
 
+#' Replace strings that duplicate the previous string
+#'
+#' The common use of this function to clean up columns in a presentation-quality
+#' table.
+#' @param string a character vector
+#' @param replacement text to use as a replacement for duplicated values
+#' @return a single character vector with immediately repeating items replaced
 #' @export
+#' @examples
+#' str_replace_same_as_previous(
+#'   c("a", "a", "a", "b" , "b", "c", "d", "d"),
+#'   ""
+#' )
 str_replace_same_as_previous <- function(string, replacement) {
   string[is_same_as_previous(string)] <- replacement
   string
--- tests/testthat/test-strings.R
+++ tests/testthat/test-strings.R
@@ -2,10 +2,15 @@ context("strings")
 
 test_that("str_tokenize() works", {
   expect_equal(str_tokenize("word"), c("w", "o", "r", "d"))
+  expect_equal(
+    str_tokenize("word   word  word", "\\s+"),
+    c("word", "word", "word")
+  )
 })
 
 test_that("str_replace_same_as_previous() works", {
   expect_equal(
     str_replace_same_as_previous(c("a", "a", "a", "b", "b", "c", "a"), "-"),
-    c("a", "-", "-", "b", "-", "c", "a"))
+    c("a", "-", "-", "b", "-", "c", "a")
+  )
 })
